ffefc30bc7c14ae7ad6e6524e234f579fb2c57c0
Brett Carrington <brettcar@gmail.com>
2009-01-18 18:25:43 -0500

    Added basic interface code for display module

---
+++ display.cpp
@@ -0,0 +1,31 @@
+#import "WProgram.h"       /* Needed for access to Serial library */
+
+typedef struct {
+  char choice[6];          /* 6-character string for this choice. No terminator, use spaces for unused characters. */
+  void (*callback)(void);  /* Callback to execute when this choice is selected */
+} CHOICE;  
+
+typedef struct {
+  char message[16];        /* 16-character string to display on top line for this menu entry. No terminator, use spaces for unused chars. */
+  CHOICE choices[2];       /* Two choices to display on second line. */
+} MENU_ENTRY;
+
+
+void display_clear(void) {
+#ifdef CLEAR_FAST
+  Serial.print("                                "); // 32 spaces
+#else
+  Serial.print (0xFE, BYTE); 
+  Serial.print (0x01, BYTE);
+#endif
+}
+
+void display_draw_entry(MENU_ENTRY entry)
+{
+  display_clear();
+  Serial.print(entry.message);
+  Serial.print("<");
+  Serial.print(entry.choices[0].choice);
+  Serial.print("  ");
+  Serial.print(entry.choices[1].choice);
+}
