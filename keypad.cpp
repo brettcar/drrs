ec2173e063bf2ac43a6883ab1546bd19422ceb6f
Brett Carrington <brettcar@gmail.com>
2009-02-17 11:04:32 -0500

    Adding keypad.cpp template for Luis

---
+++ keypad.cpp
@@ -0,0 +1,10 @@
+#import "WProgram.h"       /* Needed for access to Serial library */
+
+void keypad_isr()
+{
+ // Assumption: data is ready on keypad
+ // Read whatever is needed
+ // Call display_process if it was a * or #
+ // Else, for now, just CLEAR and then print to LCD panel
+}
+
